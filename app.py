c690ab20c5d2b75d10990425aa6f4eb2535c13b3
shishir <shisirsharma22@gmail.com>
2023-12-26 00:21:34 +0545

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import gradio as gr
+import random
+
+
+def alternatingly_agree(message, history):
+    if len(history) % 2 == 0:
+        return f"Yes, I do think that '{message}'"
+    else:
+        return "I don't think so"
+
+gr.ChatInterface(
+    alternatingly_agree,
+    chatbot=gr.Chatbot(height=300),
+    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    title="Yes Man",
+    description="Ask Yes Man any question",
+    theme="soft",
+    examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+    cache_examples=True,
+    retry_btn=None,
+    undo_btn="Delete Previous",
+    clear_btn="Clear",
+).launch()
+
+# def main():
+#     gr.ChatInterface(alternatingly_agree).launch()
+
+# if __name__=="__main__":
+#     main()
